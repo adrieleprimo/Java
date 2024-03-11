6d3b0f4208f57281ff97aae6e2decabb47c2bb87
Adriele <adriele.dejprimo@gmail.com>
2024-03-11 15:55:34 -0300

    Triangle area without Object oriented

---
+++ Class-Attributes-Statics/TriangleXAndY/src/Area.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class Area {
+    public static void main (String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        double xA, xB, xC, yA, yB, yC;
+        System.out.println("Enter the measures of triangle X");
+        xA = sc.nextDouble();
+        xB = sc.nextDouble();
+        xC = sc.nextDouble();
+        System.out.println("Enter the measures of triangle Y");
+        yA = sc.nextDouble();
+        yB = sc.nextDouble();
+        yC = sc.nextDouble();
+
+        double pX = (xA+xB+xC)/2;
+        double areaX = Math.sqrt((pX*(pX - xA)*(pX-xB)*(pX-xC)));
+        double pY = (yA + yB + yC)/2;
+        double areaY = Math.sqrt(pY*(pY-yA)*(pY-yB)*(pY-yC));
+
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n", areaY);
+        if(areaX > areaY) {
+            System.out.println("Large area: X");
+        } else {
+            System.out.println("Large area: Y");
+        }
+        sc.close();
+
+    }
+
+}
